5f1269a901f2d8ab5f1f939dc9aa5c73ca503144
Justice Isreal <justiceyba@gmail.com>
2023-03-22 01:00:28 +0100

    design: New arrival component setup

---
+++ Components/NewArrivals.jsx
@@ -0,0 +1,121 @@
+import React from "react";
+
+function NewArrivals() {
+  const prom = [
+    {
+      id: 122,
+      img: "https://res.cloudinary.com/isreal/image/upload/v1662211856/jayflix%20vid%20posters/photo_2022-09-02_18-43-21_hx0vs6.jpg",
+    },
+    {
+      id: 233,
+      img: "https://res.cloudinary.com/isreal/image/upload/v1662211845/jayflix%20vid%20posters/photo_2022-09-02_18-40-11_u8gi0i.jpg",
+    },
+    {
+      id: 42311,
+      img: "https://res.cloudinary.com/isreal/image/upload/v1662211865/jayflix%20vid%20posters/photo_2022-09-02_18-45-55_numjln.jpg",
+    },
+    {
+      id: 41342,
+      img: "https://res.cloudinary.com/isreal/image/upload/v1662211856/jayflix%20vid%20posters/photo_2022-09-02_18-43-21_hx0vs6.jpg",
+    },
+    {
+      id: 413,
+      img: "https://res.cloudinary.com/isreal/image/upload/v1662211845/jayflix%20vid%20posters/photo_2022-09-02_18-40-11_u8gi0i.jpg",
+    },
+    {
+      id: 1411,
+      img: "https://res.cloudinary.com/isreal/image/upload/v1662211865/jayflix%20vid%20posters/photo_2022-09-02_18-45-55_numjln.jpg",
+    },
+    {
+      id: 412,
+      img: "https://res.cloudinary.com/isreal/image/upload/v1662211856/jayflix%20vid%20posters/photo_2022-09-02_18-43-21_hx0vs6.jpg",
+    },
+    {
+      id: 2543,
+      img: "https://res.cloudinary.com/isreal/image/upload/v1662211845/jayflix%20vid%20posters/photo_2022-09-02_18-40-11_u8gi0i.jpg",
+    },
+    {
+      id: 32151,
+      img: "https://res.cloudinary.com/isreal/image/upload/v1662211865/jayflix%20vid%20posters/photo_2022-09-02_18-45-55_numjln.jpg",
+    },
+    {
+      id: 2532,
+      img: "https://res.cloudinary.com/isreal/image/upload/v1662211856/jayflix%20vid%20posters/photo_2022-09-02_18-43-21_hx0vs6.jpg",
+    },
+    {
+      id: 143,
+      img: "https://res.cloudinary.com/isreal/image/upload/v1662211845/jayflix%20vid%20posters/photo_2022-09-02_18-40-11_u8gi0i.jpg",
+    },
+    {
+      id: 57631,
+      img: "https://res.cloudinary.com/isreal/image/upload/v1662211865/jayflix%20vid%20posters/photo_2022-09-02_18-45-55_numjln.jpg",
+    },
+    {
+      id: 6362,
+      img: "https://res.cloudinary.com/isreal/image/upload/v1662211856/jayflix%20vid%20posters/photo_2022-09-02_18-43-21_hx0vs6.jpg",
+    },
+    {
+      id: 3463,
+      img: "https://res.cloudinary.com/isreal/image/upload/v1662211845/jayflix%20vid%20posters/photo_2022-09-02_18-40-11_u8gi0i.jpg",
+    },
+    {
+      id: 631,
+      img: "https://res.cloudinary.com/isreal/image/upload/v1662211865/jayflix%20vid%20posters/photo_2022-09-02_18-45-55_numjln.jpg",
+    },
+    {
+      id: 3622,
+      img: "https://res.cloudinary.com/isreal/image/upload/v1662211856/jayflix%20vid%20posters/photo_2022-09-02_18-43-21_hx0vs6.jpg",
+    },
+    {
+      id: 23,
+      img: "https://res.cloudinary.com/isreal/image/upload/v1662211845/jayflix%20vid%20posters/photo_2022-09-02_18-40-11_u8gi0i.jpg",
+    },
+    {
+      id: 31,
+      img: "https://res.cloudinary.com/isreal/image/upload/v1662211865/jayflix%20vid%20posters/photo_2022-09-02_18-45-55_numjln.jpg",
+    },
+    {
+      id: 52,
+      img: "https://res.cloudinary.com/isreal/image/upload/v1662211856/jayflix%20vid%20posters/photo_2022-09-02_18-43-21_hx0vs6.jpg",
+    },
+    {
+      id: 63,
+      img: "https://res.cloudinary.com/isreal/image/upload/v1662211845/jayflix%20vid%20posters/photo_2022-09-02_18-40-11_u8gi0i.jpg",
+    },
+    {
+      id: 31,
+      img: "https://res.cloudinary.com/isreal/image/upload/v1662211865/jayflix%20vid%20posters/photo_2022-09-02_18-45-55_numjln.jpg",
+    },
+  ];
+  return (
+    <div className="new-arrivals-main-con">
+      <div className="new-arrivals-con">
+        {/* heading */}
+        <h1>
+          NEW <br /> ARRIVALS
+        </h1>
+        {/* product container */}
+        <div className="new-products-con">
+          {prom.map((item, index) => (
+            <div className="new-products-case">
+              <div className="new-products">
+                <img
+                  src={item.img}
+                  alt=""
+                  key={item.id}
+                  style={{
+                    right: "0",
+                  }}
+                />
+              </div>
+              <p>200</p>
+              <p>200</p>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default NewArrivals;
--- pages/HomePage.jsx
+++ pages/HomePage.jsx
@@ -3,6 +3,7 @@ import React from "react";
 // components
 import Topbar from "../Components/Topbar";
 import Banner from "../Components/Banner";
+import NewArrivals from "../Components/NewArrivals";
 
 function HomePage() {
   return (
@@ -12,61 +13,7 @@ function HomePage() {
       {/* BANNER */}
       <Banner />
       {/* NEW ARRIVALS */}
-      <p>
-        Skip to main contentAccessibility help Accessibility feedback Google
-        next js pwa set up in 2023 All NewsVideosImagesBooksMore Tools About
-        618,000 results (0.50 seconds) Videos 13:08 How to Create a PWA With
-        Next.js in 10 Minutes YouTube · James Q Quick 5 Oct 2021 9 key moments
-        in this video 3:26 Progressive Web App in Next.js YouTube · Benjamin
-        Carlson 2 Jan 2022 7 key moments in this video View all Turn your
-        Next.js application into a PWA - MakerKit MakerKit https://makerkit.dev
-        › blog › tutorials › pwa-nextjs 22 Sept 2022 — PWA can make your app
-        look native, faster, updatable and offline-ready. In this post, we learn
-        how to make a PWA with a Next.js application. PWA with Next.js - DEV
-        Community DEV Community https://dev.to › anuraggharat ›
-        pwa-with-nextjs-5178 24 Mar 2022 — What is PWA? PWA stands for
-        Progressive Web Apps. A PWA Web App can be installed as an Application
-        on any device and work as a Native App on ... People also ask Can I
-        build PWA with next JS? How to implement PWA in NextJS? Is NextJS good
-        for web apps? Can I publish PWA to App Store? Feedback Build a
-        Progressive Web App (PWA) with Next.js - Refine Dev Refine Dev
-        https://refine.dev › blog › next-js-pwa 11 Sept 2022 — We will walk you
-        through the entire process of building a PWA using Next.JS and refine
-        framework, from start to finish! ‎Introduction · ‎What is a PWA? ·
-        ‎Adding Tailwind CSS for styling · ‎Using Next.js SSR How to Create a
-        PWA With Next.js - Avneesh Agarwal Avneesh Agarwal
-        https://blog.avneesh.tech › how-to-create-a-pwa-with-... 4 Oct 2021 — I
-        am going to convert the default Next.js template into a PWA, you can
-        convert your web app. Installing the required dependency. Copy. How to
-        build and deploy a PWA with Next.js - Bird Eats Bug Bird Eats Bug
-        https://birdeatsbug.com › blog › build-and-deploy-a-p... 18 Jul 2022 —
-        Install the Progressive Web App dependency · Create or generate a
-        manifest file · Create a document.js file · Configure the next.config.js
-        file ... Build a PWA with GraphCommerce and Next.js LogRocket
-        https://blog.logrocket.com › build-pwa-graphcommer... 7 Sept 2022 — You
-        can see the configuration in your next.config.js . Inside you should
-        see: const nextConfig = ... People also search for next js pwa example
-        next js pwa tutorial next-pwa not working next js pwa offline next-pwa
-        github pwa next An ultimate guide to build PWA with Next JS - SoluteLabs
-        SoluteLabs https://www.solutelabs.com › blog › ultimate-guide-to... 19
-        Nov 2021 — Simply put, PWA is a website or application that delivers
-        native-like app performance. It is fast, reliable, and engaging. Adriana
-        Jara, a ... People also search for next js pwa example next js pwa
-        tutorial next-pwa not working next js pwa offline next-pwa github pwa
-        next Progressive Web App (PWA) in Next.js - Bionic Julia Bionic Julia
-        https://bionicjulia.com › blog › progressive-web-app-... How to create a
-        PWA in Next.js · Install the package with yarn add next-pwa . · If you
-        don't already have one, create a manifest.json file in your /public
-        folder. Building Progressive Web Apps with NEXT.js - Dialectica
-        Dialectica https://dialecticanet.com › blog › building-progressive... 6
-        Dec 2022 — Last but not least, we must configure the PWA dependency by
-        adding the following code in our next.config.js file. ... dest:
-        “public”, register: ... Related searches next js pwa example next js pwa
-        tutorial next-pwa not working next js pwa offline next-pwa github pwa
-        next next-pwa push notification npm next-pwa 1 2 3 4 5 6 7 8 9 10 Next
-        Nigeria 500272, Port Harcourt - Based on your past activity - Update
-        location HelpSend feedbackPrivacyTerms
-      </p>
+      <NewArrivals />
       {/* MAIN PRODUCT */}
 
       {/* SUBSCRIBE */}
