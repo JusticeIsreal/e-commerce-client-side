1cfe46e7ca7517f9aa3254055f4e9c9154f745f8
Justice Isreal <justiceyba@gmail.com>
2023-03-25 10:56:23 +0100

    design: changed products to shoes

--- Components/Homepage/Banner.jsx
+++ Components/Homepage/Banner.jsx
@@ -3,25 +3,25 @@ import { Fade, Slide } from "react-slideshow-image";
 function Banner() {
   const promo = [
     {
-      id: 233,
+      id: 3,
       img: "https://res.cloudinary.com/isreal/image/upload/v1679725095/E-Commerce%20Project/aaron-burden-9RpDzXTlNh8-unsplash_syzdew.jpg",
       cat: "Classic Pack",
       desc: "Classic pack for you classic look, wallets, glasses, pure leather belt, tie and great scent. we got you covereed for classic look",
     },
     {
-      id: 143,
+      id: 4,
       img: "https://res.cloudinary.com/isreal/image/upload/v1679725028/E-Commerce%20Project/joseph-greve-dPw0N01onxE-unsplash_qiayzv.jpg",
       cat: "Sun shades",
       desc: "Perfect sun shade for that summer outing, keep you on the VIP list and great personality on the 001 sun shade",
     },
     {
-      id: 17650762,
+      id: 1,
       img: "https://res.cloudinary.com/isreal/image/upload/v1679725015/E-Commerce%20Project/daniel-storek-JM-qKEd1GMI-unsplash_tghekk.jpg",
       cat: "Shoes",
       desc: "Your number 1 go to shop for all the standard shoe fit, strong durable and original to keep you on top of your game",
     },
     {
-      id: 172,
+      id: 2,
       img: "https://res.cloudinary.com/isreal/image/upload/v1679725083/E-Commerce%20Project/maarten-van-den-heuvel-1t-sb-SgAxs-unsplash_k9fblx.jpg",
       cat: "Phones",
       desc: "gardget and  obile accessories for all the standard shoe fit, strong durable and original to keep you on",
@@ -50,7 +50,6 @@ function Banner() {
               {promo.map((item, index) => (
                 <div className="banner-img-con" key={item.id}>
                   <div className="div-2">
-                    
                     <img
                       src={item.img}
                       alt=""
