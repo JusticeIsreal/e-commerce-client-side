f3668c7c9e2c8af21e4372c9c2194e6bf42e854d
Justice Isreal <justiceyba@gmail.com>
2023-04-02 00:51:34 +0100

    LOGIC: dynamic page image re adjust size

--- pages/ClientDynamic/[productID].jsx
+++ pages/ClientDynamic/[productID].jsx
@@ -11,6 +11,9 @@ import {
 import { db } from "../../Firebase";
 import Image from "next/image";
 
+// ICONS
+import { FiMinusCircle, FiPlusCircle } from "react-icons/fi";
+
 export async function getStaticPaths() {
   const colRef = collection(db, "products");
   const snapshot = await getDocs(colRef);
@@ -41,18 +44,18 @@ function Details({ product }) {
   const router = useRouter();
   const { productID } = router.query;
   const pic = useRef();
-  console.log(product);
+  // console.log(product);
 
   const [disimg, setDisimg] = useState(0);
   const changeIMG = (index) => {
     setDisimg(index);
-    console.log(pic.current.classList);
+    // console.log(pic.current.classList);
   };
   return (
     <div className="client-single-product">
       <div className="single-product">
         <div className="big-display-con">
-          {" "}
+         
           <div className="big-display-img">
             <Image src={product.image[disimg]} alt="img" fill sizes="100vw" />
           </div>
@@ -74,6 +77,44 @@ function Details({ product }) {
                 </div>
               )
           )}
+        </div>
+      </div>
+      <div>
+        <h1>{product.productname}</h1>
+        <p>{product.productnumber}</p>
+        <p>{product.productdescription}</p>
+        <div>
+          <div>
+            {" "}
+            <span>
+              <FiMinusCircle />
+            </span>
+            <h3>1</h3>
+            <span>
+              <FiPlusCircle />
+            </span>
+          </div>
+
+          <h1>₦ {product.productprice}</h1>
+        </div>
+        <div>
+          <div>Add to cart</div>
+          <div>Buy Noe</div>
+          <div>View in Cart</div>
+        </div>
+
+        <div>
+          <h3>REVIEW</h3>
+          <div>
+            <div>
+              <button>Add Review in position absolute</button>
+            </div>
+            <div>
+              <p>Date / time</p>
+              <p>Review text using mantine</p>
+              <p>User name</p>
+            </div>
+          </div>
         </div>
       </div>
     </div>
