ed27fd5f6c49a22511c8f1c3cd30919e427a0c18
Justice Isreal <justiceyba@gmail.com>
2023-04-01 23:45:52 +0100

    LOGIC: dynamic page bug fixed

--- pages/ClientDynamic/[productID].jsx
+++ pages/ClientDynamic/[productID].jsx
@@ -18,7 +18,7 @@ export async function getStaticPaths() {
     params: { productID: doc.id },
   }));
 
-  return { paths, fallback: true };
+  return { paths, fallback: "blocking" };
 }
 
 export const getStaticProps = async ({ params }) => {
@@ -41,7 +41,7 @@ function Details({ product }) {
   const router = useRouter();
   const { productID } = router.query;
   const pic = useRef();
-  // console.log(product);
+  console.log(product);
 
   const [disimg, setDisimg] = useState(0);
   const changeIMG = (index) => {
