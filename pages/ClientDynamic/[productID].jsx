bcd7eebe7d29f18da5f658f8ed22689647c599f0
Justice Isreal <justiceyba@gmail.com>
2023-04-02 15:09:10 +0100

    DESIGN: single product review mobile view similar product listings

--- pages/ClientDynamic/[productID].jsx
+++ pages/ClientDynamic/[productID].jsx
@@ -1,5 +1,7 @@
+import Loader from "../../Components/Loader";
 import { useEffect, useState, useRef } from "react";
 import { useRouter } from "next/router";
+import Link from "next/link";
 import {
   collection,
   getDocs,
@@ -7,6 +9,8 @@ import {
   getDoc,
   query,
   orderBy,
+  where,
+  onSnapshot,
 } from "firebase/firestore";
 import { db } from "../../Firebase";
 import Image from "next/image";
@@ -59,12 +63,34 @@ function Details({ product }) {
   function goBack() {
     router.back();
   }
+
+  // simillar product
+
+  const [similarProducts, setSimilarProducts] = useState([]);
+  useEffect(() => {
+    return onSnapshot(
+      query(
+        collection(db, "products"),
+        where("productcategory", "==", `${product.productcategory}`)
+      ),
+      (snapshot) => {
+        setSimilarProducts(
+          snapshot.docs.filter(
+            (item) => item.category !== `${product.productcategory}`
+          )
+        );
+      }
+    );
+  }, [db]);
+  console.log(similarProducts);
+  // console.log(product.productcategory);
   return (
     <div className="client-single-product">
       <div className="single-product">
         <div className="big-display-con">
           <button onClick={goBack} className="go-back">
-            <TiArrowBack />Back
+            <TiArrowBack />
+            Back
           </button>
           <div className="big-display-img">
             <Image
@@ -170,9 +196,74 @@ function Details({ product }) {
             </div>
           </div>
         </div>
+        {/* similar products */}
+
+        {similarProducts.length < 1 ? (
+          <Loader />
+        ) : (
+          <div className="similar-products">
+            {similarProducts.map((product) => (
+              <SimilarProducts
+                key={product.id}
+                id={product.id}
+                productimages={product.data().image}
+                productname={product.data().productname}
+                productprice={product.data().productprice}
+                productoldprice={product.data().productoldprice}
+              />
+            ))}
+          </div>
+        )}
       </div>
     </div>
   );
 }
 
 export default Details;
+
+function SimilarProducts({
+  id,
+  productimages,
+  productname,
+  productprice,
+  productoldprice,
+}) {
+  return (
+    <div className="products">
+      <div className="product-img">
+        <Link
+          href="/ClientDynamic/[productID]"
+          as={`/ClientDynamic/${id}`}
+          style={{ width: "100%", display: "flex", justifyContent: "center" }}
+        >
+          <div style={{ width: "100%", height: "100%", position: "relative" }}>
+            <img
+              src={productimages[0]}
+              alt="img"
+              className="home-product-img"
+              // style={{
+              //   width: "100px",
+              // }}
+              // fill
+              // sizes="100vw"
+            />
+          </div>
+        </Link>
+      </div>
+      <p className="product-name">{productname}</p>
+      <div className="price">
+        <p className="product-price">
+          ₦ {Number(productprice).toLocaleString()}
+        </p>
+        <p className="product-oldprice">
+          {" "}
+          {productoldprice && "₦ " + Number(productoldprice).toLocaleString()}
+        </p>
+      </div>
+
+      <Link href="/" className="addto-cart">
+        Add to cart
+      </Link>
+    </div>
+  );
+}
