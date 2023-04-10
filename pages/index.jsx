dcbda538f557980ae0a46f1075e5ea46bff74e8c
Justice Isreal <justiceyba@gmail.com>
2023-04-11 00:12:19 +0100

    LOGIC: admin route authentication

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,11 +1,9 @@
 import Head from "next/head";
 import Image from "next/image";
 import { Inter } from "next/font/google";
-import Homepage from "./Homepage";
-
+// import Homepage from "./Homepage";
 
 // PAGES
-
 
 const inter = Inter({ subsets: ["latin"] });
 
