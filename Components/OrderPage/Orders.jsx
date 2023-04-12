3c358e069f2f0f3cfc9c6ba29e657d69e2bc1c95
Justice Isreal <justiceyba@gmail.com>
2023-04-12 02:23:54 +0100

    DESIGN: Client order page responsive set up

--- Components/OrderPage/Orders.jsx
+++ Components/OrderPage/Orders.jsx
@@ -1,36 +1,68 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
+// icon
+import { FaMoneyCheck } from "react-icons/fa";
+import { getSessionUser } from "../../Services/functions";
+import { useRouter } from "next/router";
 function Orders() {
+  // FETCHING SESSION USER NAME AND CART LENGTH
+
+  const router = useRouter();
+  const [userTransaction, setUserTransaction] = useState([]);
+  useEffect(() => {
+    const userName = async () => {
+      const userData = await getSessionUser();
+      setUserTransaction(userData?.user?.transaction);
+    };
+    userName();
+  }, [router]);
+  console.log(userTransaction);
+
   return (
     <div className="oders-con">
       <div className="order-page-top">
         <h1>TRANSACTIONS</h1>
         <div className="order-status">
-          <p>Pending</p>
+          <p>All</p>
           <p>Processing</p>
           <p>Transit</p>
           <p>Delievered</p>
         </div>
       </div>
-      <TransactionReceipt />
+      <div className="each-order-con">
+        {userTransaction.map((order) => (
+          <TransactionReceipt key={order._id} {...order} />
+        ))}
+      </div>
     </div>
   );
 }
 
 export default Orders;
 
-function TransactionReceipt() {
+function TransactionReceipt({ timestamp, totalAmount, status }) {
+  // conver time stamp
+  function formatDate(dateString) {
+    const date = new Date(dateString);
+    const options = { day: "numeric", month: "long", year: "numeric" };
+    return date.toLocaleDateString("en-US", options);
+  }
+
+  const formattedTimestamp = formatDate(timestamp);
+
   return (
     <div className="each-order">
-      <div className="order-icon">icon</div>
+      <div className="order-icon">
+        <FaMoneyCheck />
+      </div>
       <div className="order-details">
-        <p>Date</p>
-        <p>Product</p>
-        <p>Total</p>
-        <p>payment status</p>
+        <p>{formattedTimestamp}</p>
+        <p>{"Producct"}</p>
+        <p>₦ {totalAmount.toLocaleString()}</p>
+        <p>{status}</p>
       </div>
       <div className="order-payment-status">
-        <p>status</p>
+        <p>{status}</p>
       </div>
     </div>
   );
--- pages/orders.jsx
+++ pages/orders.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Topbar from "../Components/Topbar";
 import Orders from "../Components/OrderPage/Orders";
+import Footer from "../Components/Footer";
 
 function orders() {
   return (
     <div className="order-page-main-con">
       <Topbar />
       <Orders />
+      <Footer />
     </div>
   );
 }
